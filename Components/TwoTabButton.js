87e234644fd1ad38d5270602ea17576162957970
Paula Gambús <paulinagambus@gmail.com>
2024-04-24 16:35:25 +0200

    twoTabButton edit

--- Components/TwoTabButton.js
+++ Components/TwoTabButton.js
@@ -2,20 +2,26 @@ import React, { useState } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { colors } from "../Screens/Utils/Colors";
 
-const TwoTabButton = ({ text1, text2 }) => {
+const TwoTabButton = ({ text1, text2, onPress }) => {
   const [activeTab, setActiveTab] = useState(1); // Initial tab is 1
 
   return (
     <View style={styles.container}>
       <TouchableOpacity
         style={[styles.tab, activeTab === 1 ? styles.activeTab : null]}
-        onPress={() => setActiveTab(1)}
+        onPress={() => {
+          setActiveTab(1);
+          onPress('week'); // Pass 'week' when text1 is pressed
+        }}
       >
         <Text style={styles.tabText}>{text1}</Text>
       </TouchableOpacity>
       <TouchableOpacity
         style={[styles.tab, activeTab === 2 ? styles.activeTab : null]}
-        onPress={() => setActiveTab(2)}
+        onPress={() => {
+          setActiveTab(2);
+          onPress('month'); // Pass 'month' when text2 is pressed
+        }}
       >
         <Text style={styles.tabText}>{text2}</Text>
       </TouchableOpacity>
