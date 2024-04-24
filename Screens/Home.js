aa7666f14ad5ddbba139db9e5d818885adde675c
Lidia Perpiñán <s217151@student.dtu.dk>
2024-04-24 15:56:42 +0200

    home

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Alert, TouchableHighlight, SafeAreaView, Image, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Alert, TouchableHighlight, SafeAreaView, Image, Button, TouchableOpacity, Dimensions } from 'react-native';
 import { colors } from "./Utils/Colors";
 
 
@@ -6,23 +6,26 @@ export default function HomePage({navigation}) {
 
     // Handler of the add button
     const pressHandler = () => {
+      console.log('button 1 pressed');
       navigation.navigate('HomePage');
     }
 
-    const loginPressed = () => {
-      Alert.alert('Login button pressed');
-    }
+    //const loginPressed = () => {
+      //Alert.alert('Login button pressed');
+    //}
     
     return (
-      // the second style is to override the first style
-      <SafeAreaView style={[styles.container1]}>
-        <Image
-          source={require('../assets/logo.png')}
-          style={styles.logo}
-          resizeMode='contain'
-          />
-      </SafeAreaView>
+      <View>
+        <SafeAreaView>
+          <TouchableOpacity onPress={pressHandler}>
+            <View>
+              <Text>Let's go!</Text>
+            </View>
+          </TouchableOpacity>
+        </SafeAreaView>
+      </View>
     );
+    
   }
   
   const styles = StyleSheet.create({
@@ -37,7 +40,7 @@ export default function HomePage({navigation}) {
       width: '50%',
       height: '50%',
       position: 'absolute',
-      marginTop: '20%',
+      //marginTop: '1%',
       marginLeft: '25%',
       marginRight: '25%'
       // top: 60, // Adjust this value to position the logo vertically
@@ -47,25 +50,46 @@ export default function HomePage({navigation}) {
     button: {
       width: "50%",
       borderRadius: 25,
-      marginVertical: 320,
+      marginTop: '90%',
       alignItems: 'center',
       marginLeft: '24%',
       paddingHorizontal: 10,
     },
     
+    button2: {
+      width: "50%",
+      borderRadius: 25,
+      marginTop: '10%',
+      alignItems: 'center',
+      marginLeft: '24%',
+      paddingHorizontal: 10,
+    },
+
     buttonInner: {
       backgroundColor: colors.DARK_PURPLE,
       paddingVertical: 15,
       borderRadius: 25,
+      marginTop: '-10%',
       width: '100%',
       alignItems: 'center',
     },
   
+    buttonInner2: {
+      backgroundColor: colors.DARK_PURPLE,
+      paddingVertical: 15,
+      borderRadius: 25,
+      marginTop: '100%',
+      width: '100%',
+      alignItems: 'center',
+    },
+
     buttonText: {
       color: colors.WHITE,
       fontWeight: 'bold',
       alignItems: 'center',
       fontSize: 16,
     },
+
+  
   });
   
